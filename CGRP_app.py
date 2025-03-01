2e93d52ed3a797cab345e18882fdf000fe66516a
danigeiger <145297984+danigeiger@users.noreply.github.com>
2025-02-28 23:23:13 -0800

    Update CGRP_app.py

--- CGRP_app.py
+++ CGRP_app.py
@@ -203,7 +203,7 @@ with tab2:
     st.write("## **Education**")
     st.write("""
     - **Bachelor of Science**, University of Oregon (2017-2021)  
-      *Focus: Human Physiology, Biochemistry,Applications in Medicine  *
+      *Focus: Human Physiology, Biochemistry, Applications in Medicine*
     """)
 
     # Experience Section
